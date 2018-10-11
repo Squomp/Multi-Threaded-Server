a9810d3453a76c3bb9f68daa7fc50a6d2a36436d
Nathan <NParsons@student.neumont.edu>
2018-10-11 10:18:12 -0600

    Added basic classes.

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,20 @@
+package server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	ServerSocket ss;
+	Socket client;
+	int port = 0;
+	
+	public Server() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	class ServerThread {
+		
+	}
+
+}
