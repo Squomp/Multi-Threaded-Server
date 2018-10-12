6c9f2a8079c34794ece92bdb0667cb2bb7a65aa7
Nathan <NParsons@student.neumont.edu>
2018-10-12 11:08:47 -0600

    Added threading to client for accepting messages. Server now sends a message to all clients currently connected. formatting is horrendous.

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -8,18 +8,21 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-	
+
 	public Client() {
 		Socket s;
 		try {
 			s = new Socket("localhost", 3001);
 			PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-			String message = "yeet";
-			out.println(message);
-			System.out.println("Sent: " + message);
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			String answer = in.readLine();
-			System.out.println("Server said: " + answer);
+			System.out.println("Address: " + s.getLocalSocketAddress());
+			new ClientAcceptMessageThread(in).start();
+			while (true) {
+				System.out.print("Type a message: ");
+				String message = br.readLine();
+				out.println(message);
+			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -27,4 +30,25 @@ public class Client {
 		}
 	}
 
+	class ClientAcceptMessageThread extends Thread {
+
+		BufferedReader in;
+
+		public ClientAcceptMessageThread(BufferedReader in) {
+			this.in = in;
+		}
+
+		@Override
+		public void run() {
+			String answer;
+			while (true) {
+				try {
+					answer = in.readLine();
+					System.out.println(answer);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -6,60 +6,85 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-	
+
 	public static final int THREAD_COUNT = 10;
 	private ServerSocket ss;
 	private Socket client;
 	private int port = 3001;
 	private ExecutorService es;
-	
+	private static List<Socket> clients = new ArrayList<Socket>();
+
 	public Server() {
 		es = Executors.newFixedThreadPool(THREAD_COUNT);
+		try {
+			ss = new ServerSocket(port);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		while (true) {
-			ServerSocket serverSocket;
 			try {
-				serverSocket = new ServerSocket(port);
-				client = serverSocket.accept();
+				System.out.println(ss.getInetAddress().toString());
+				client = ss.accept();
 				// start thread
-				es.execute(new ServerThread(client));
+				Thread t = new ServerThread(client);
+				clients.add(client);
+				es.execute(t);
+				System.out.println("here");
 			} catch (IOException e) {
-//				e.printStackTrace();
+				// e.printStackTrace();
 			}
 		}
 	}
-	
+
 	class ServerThread extends Thread {
-		
+
 		private Socket client;
 		private PrintWriter out;
 		private BufferedReader in;
-		
-		
+
 		public ServerThread(Socket client) {
 			this.client = client;
 			try {
-				this.out = new PrintWriter(client.getOutputStream(), true);
 				this.in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		@Override
 		public void run() {
 			try {
 				String message = in.readLine();
-				out.println(message);
+				System.out.println("Recieved " + message);
+				sendMessage(client.getRemoteSocketAddress() + " " + message);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
+		private void sendMessage(String message) {
+			System.out.println("Number of clients: " + clients.size());
+			for (Socket s : clients) {
+				if (!s.equals(client)) {
+					try {
+						out = new PrintWriter(s.getOutputStream(), true);
+						out.println(message);
+						out.flush();
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
 	}
 
 }
